e82b41c5a0e25e8d02b1d8f05a83d7651d1f4cbd
scarecrowcxc <scarecrow_cxc@hotmail.com>
2019-01-19 09:50:36 +0800

    users serializers.py

--- apps/users/serializers.py
+++ apps/users/serializers.py
@@ -1,8 +1,34 @@
 from rest_framework import serializers
-from .models import VerifyCode
+from .models import VerifyCode, UserProfile
+from gulishop.setting import MOBILE_RE
+from datetime import datetime
+import re
 
 
 class VerifyCodeSerializer(serializers.ModelSerializer):
+
+    def validate_mobile(self, mobile):
+        # 第一步 ： 判断手机是否合法
+
+        com = re.compile(MOBILE_RE)
+        if not com.match(mobile):
+            raise serializers.ValidationError('手机号码不合法')
+
+        # 第二步：判断手机是否注册过
+        if UserProfile.objects.filter(mobile=mobile):
+            raise serializers.ValidationError('手机号码已经被注册')
+        # 第三步：判断手机发送验证码频率
+        ver_list = VerifyCode.objects.filter(mobile=mobile).order_by('-add_time')
+        if ver_list:
+            ver = ver_list[0]
+            if datetime.now() - ver.add_time.second <= 60:
+                raise serializers.ValidationError('验证码已发送，请1分钟后再次发送')
+            else:
+                # 把作废的验证码删掉
+                ver.delete()
+        return mobile
+
+
     class Meta:
         model = VerifyCode
         fields = ['mobile']
