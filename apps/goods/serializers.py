8dda2b164120d0a48fae40a06b6b7e976bfcfe32
scarecrowcxc <scarecrow_cxc@hotmail.com>
2019-01-20 11:27:50 +0800

    serializers.py finished

--- apps/goods/serializers.py
+++ apps/goods/serializers.py
@@ -1,22 +1,20 @@
 from rest_framework import serializers
 from .models import Goods
 
-# 对于前后端分离项目来说 这个文件很重要
 
 
-# 这是最痛快的那种写法
+# class GoodsSerializer(serializers.Serializer):
+#     name = serializers.CharField(required=True, max_length=30, min_length=3)
+#     goods_front_image = serializers.ImageField(required=True)
+#     shop_price = serializers.FloatField(required=True)
+#     add_time = serializers.DateTimeField(required=True)
+
+
 class GoodsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Goods
         fields = '__all__'
 
-        # 也可以部分序列化
-        # fields = ['name','add_time']
+        # # 还可以部分显示
+        # fields = ['name', 'add_time']
 
-
-
-class GoodsSerializer(serializers.Serializer):
-    name = serializers.CharField(required=True, max_length=30, min_length=3)
-    goods_front_image = serializers.ImageField(required=True)
-    shop_price = serializers.FloatField(required=True)
-    add_time = serializers.DateTimeField(required=True)
