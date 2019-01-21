79287d86a4b3f5f1d6122669cdfc58b99c95d207
scarecrowcxc <scarecrow_cxc@hotmail.com>
2019-01-21 15:12:44 +0800

    use GenericAPIView

--- apps/goods/views.py
+++ apps/goods/views.py
@@ -10,6 +10,8 @@ from .serializers import GoodsSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework import generics, mixins
+
 
 # 第一种方式， 自己用python最原始的方式去序列化，但是对于图片序列化的时候， 不支持json序列化
 # class GoodsView(View):
@@ -22,12 +24,12 @@ from rest_framework import status
 #             item['shop_price'] = goods.shop_price
 #             # item['goods_front_image'] = goods.goods_front_image
 #             items.append(item)
-        # 使用HttpResponse返回 需要先去把python的json格式类型转化为json的字符串
-        # data = json.dumps(items)
-        # return HttpResponse(data, content_type='application/json')
+# 使用HttpResponse返回 需要先去把python的json格式类型转化为json的字符串
+# data = json.dumps(items)
+# return HttpResponse(data, content_type='application/json')
 
-        # 使用JsonResponse
-        # return JsonResponse(items, safe=False)
+# 使用JsonResponse
+# return JsonResponse(items, safe=False)
 
 # 第二种方式, 使用Django当中封装的model_to _dict方法一次性全部序列化， 但是他只是对我们的第一种方式的简化 并未解决图片序列化不了的问题
 # class  GoodsView(View):
@@ -40,7 +42,7 @@ from rest_framework import status
 #         # data = json.dumps(items)
 #         # return HttpResponse(data, content_type='application/json')
 #
-        # return JsonResponse(items, safe=False)
+# return JsonResponse(items, safe=False)
 
 
 # 第三种方式， 使用Django的serializers去做序列化， 它就已经帮我们把图片无法序列化的问题解决了
@@ -55,17 +57,25 @@ from rest_framework import status
 #         return JsonResponse(data, safe=False)
 
 
+######引入 Django REST framework ###### 使用更NB的View
+# class GoodsView(APIView):
+#     '''
+#         使用drf最基础的APIView实现商品列表的数据接口
+#     '''
+#
+#     def get(self, request):
+#         # 拿到要返给前端的数据
+#         all_goods = Goods.objects.all()
+#
+#         # 进行序列化 第一个参数是待序列化的数据，要求queryset类型。 得到一个序列化好后的对象实例
+#         serializer = GoodsSerializer(all_goods, many=True)
+#
+#         return Response(data=serializer.data, status=status.HTTP_200_OK)
 
-######引入Django REST framework ############使用更NB的View
-class GoodsView(APIView):
-   def get(self, request):
 
-       # 拿到要返给前端的数据
-       all_goods = Goods.objects.all()
+class GoodsView(mixins.ListModelMixin, generics.GenericAPIView):
+    queryset = Goods.objects.all()
+    serializer_class = GoodsSerializer
 
-       # 进行序列化 第一个参数是待序列化的数据，要求queryset类型。 得到一个序列化好后的对象实例
-       serializer = GoodsSerializer(all_goods, many=True)
-
-       return Response(data=serializer.data, status=status.HTTP_200_OK)
-
-
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
