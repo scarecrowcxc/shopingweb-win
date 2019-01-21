1a94c7df31e98830ed0d9d44827fd7bdeb630700
scarecrowcxc <scarecrow_cxc@hotmail.com>
2019-01-22 00:09:47 +0800

    serializers.py update

--- apps/goods/serializers.py
+++ apps/goods/serializers.py
@@ -1,6 +1,5 @@
 from rest_framework import serializers
-from .models import Goods
-
+from .models import Goods, GoodsCategory
 
 
 # class GoodsSerializer(serializers.Serializer):
@@ -18,3 +17,27 @@ class GoodsSerializer(serializers.ModelSerializer):
         # # 还可以部分显示
         # fields = ['name', 'add_time']
 
+
+
+class CategorySerializer3(serializers.ModelSerializer):
+    class Meta:
+        model = GoodsCategory
+        fields = '__all__'
+
+
+
+# 这个类用来序列化sub_cat这个二级类数据
+class CategorySerializer2(serializers.ModelSerializer):
+    sub_cat = CategorySerializer3(many=True)
+    class Meta:
+        model = GoodsCategory
+        fields = '__all__'
+
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    #把这个sub_cat往这一写，当时候就会自动的拿他的二级类别（所有都拿到）， 名字不能乱写 是related_name
+    sub_cat = CategorySerializer2(many=True)
+    class Meta:
+        model = GoodsCategory
+        fields = '__all__'
